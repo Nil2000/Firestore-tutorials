f59914c3fe438b5c0642e4ee845631aef1a61305
Nilabhra <nilabhra09b.net@gmail.com>
2021-07-19 21:36:24 +0530

    Setting up Firebase and first document added

--- app/src/main/java/com/example/firebasefirestorebasicsapplication/MainActivity.java
+++ app/src/main/java/com/example/firebasefirestorebasicsapplication/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.firebasefirestorebasicsapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MainActivity";
+    private static final String KEY_TITLE="Title";
+    private static final String KEY_DESCRIPTION="Description";
+    private EditText etTitle,etDescription;
+    Button btnSave;
+
+    //Firestore
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTitle=findViewById(R.id.etTitle);
+        etDescription=findViewById(R.id.etDescription);
+        btnSave=findViewById(R.id.btnSave);
+
+//        btnSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Here That will happen for clicking button
+//            }
+//        });
+    }
+
+    public void saveNote(View v){
+        String title=etTitle.getText().toString();
+        String description=etDescription.getText().toString();
+
+        Map<String,Object> note=new HashMap<>();
+        note.put(KEY_TITLE,title);
+        note.put(KEY_DESCRIPTION,description);
+
+        db.collection("Notebook").document("My first Note")
+                .set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Save Failed", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
     }
 }
